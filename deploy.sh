083b5da321181217d22688a61acf9a1ff932fd3f
Vandal-William <vandal.william@hotmail.com>
2024-07-06 17:07:25 +0200

    Fixe deployment script

--- deploy.sh
+++ deploy.sh
@@ -32,9 +32,24 @@ sudo rm -rf $DEST_FRONT_MANAGEMENT
 sudo mkdir -p $DEST_FRONT_MANAGEMENT
 sudo cp -r $SOURCE_DIR/front-management/dist/* $DEST_FRONT_MANAGEMENT/
 
+# Fonction pour copier le package.json
+copy_package_json() {
+  local source_dir=$1
+  local dest_dir=$2
+  cp $source_dir/package.json $dest_dir/package.json
+}
+
+# Copier le package.json de l'API Business
+copy_package_json "$SOURCE_DIR/api-business" "$DEST_API_BUSINESS"
+
+# Copier le package.json de l'API Publication
+copy_package_json "$SOURCE_DIR/api-publication" "$DEST_API_PUBLICATION"
+
 # Redémarrer les applications avec PM2
 cd $DEST_API_BUSINESS/src
+npm install
 pm2 restart main.js --name api-business || pm2 start main.js --name api-business
 
 cd $DEST_API_PUBLICATION/src
+npm install
 pm2 restart main.js --name api-publication || pm2 start main.js --name api-publication
